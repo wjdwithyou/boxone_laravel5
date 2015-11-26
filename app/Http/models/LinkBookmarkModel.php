5590526ad7644c9211b26e5416ff01f11fb1c852
Jung <jms3425@naver.com>
2015-11-26 16:01:57 +0900

    model update

---
+++ app/Http/models/LinkBookmarkModel.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ *  북마크 관련 컨트롤러
+ */
+$rootpath = "/var/www/laravel/app";
+//$rootpath = "C:/app/app";
+include $rootpath."/function/baseFunction.php";
+
+    /*  	
+     *	북마크정보 등록 기능
+     */
+	public function create($member_idx, $website_link)
+	{
+
+		if(	!(	inputErrorCheck($member_idx, 'member_idx')
+				&& inputErrorCheck($website_link, 'website_link')))
+			return ;		
+				
+		$result = DB::table('link_bookmark')->insertGetId(
+			array(
+				'member_idx'=> $member_idx, 
+				'website_link'=> $website_link, 
+				'upload'=>DB::raw('now()')
+				)
+			);	
+
+		return array('code' => 1,'msg' =>'success' ,'data' => $result);
+	}
+
+    /*  	
+     *	북마크 리스트 가져오는 기능
+     */
+	public function getInfoList()
+	{
+		$result = DB::select('select * from link_bookmark where order by idx DESC');
+
+		return array('code' => 1, 'msg' => 'success', 'data' => $result);
+	}
+
+ 	/*  	
+     *	북마크 삭제 기능
+     */
+	public function delete($idx)
+	{
+		if(	!(	inputErrorCheck($idx, 'idx')))
+			return ;		
+
+ 		$result = DB::delete('delete from link_bookmark where idx=?', array($idx));
+
+		if($result == true){
+         	return array('code' => 1, 'msg' => 'data delete success');
+        }else{
+         	return array('code' => 0, 'msg' => 'delete false: no matched data');
+        }
+	}
