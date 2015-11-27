0ca992bb2c499d8d1faf2cedde5624f87aa55f05
박용호 <dydzlwwkd@nate.com>
2015-11-27 17:45:41 +0900

    dd

--- resources/views/cardInfo.php
+++ resources/views/cardInfo.php
@@ -7,24 +7,39 @@
 	<div class="result_sub_title">
 		<?= $search?>
 	</div>
-	<?php foreach ($result as $cardList): ?>
-		<div class="result_content">
-			<div class="col-xs-4">
-				<img src="<?= $adr_img?>card/<?= $cardList->idx?>.png">
-			</div>
-			<div class="col-xs-8">
-				<div class="card_desc">
-					<div class="card_name"><?= $cardList->title?></div>
-					<div class="card_title"><?= $cardList->support_site?></div>
-					<div class="card_content">
-						<?= $cardList->contents?> <br>
-						연회비 : <?= $cardList->anual_fee?>
+	<?php 
+		if (count($result) != 0)
+		{
+			foreach ($result as $cardList): 
+	?>
+				<div class="result_content">
+					<div class="col-xs-4">
+						<img src="<?= $adr_img?>card/<?= $cardList->idx?>.png">
 					</div>
+					<div class="col-xs-8">
+						<div class="card_desc">
+							<div class="card_name"><?= $cardList->title?></div>
+							<div class="card_title"><?= $cardList->support_site?></div>
+							<div class="card_content">
+								<?= $cardList->contents?> <br>
+								연회비 : <?= $cardList->anual_fee?>
+							</div>
+						</div>
+					</div>
+					<div class="clear_both"></div>
 				</div>
+	<?php 
+			endforeach;
+		}
+		else 
+		{
+	?>
+			<div class="result_content" style="text-align:center; background-color:#DDDDDD;">
+				겁색된 결과가 없습니다!! 다시!! 입력해주세요!!
 			</div>
-			<div class="clear_both"></div>
-		</div>
-	<?php endforeach;?>
+	<?php 
+		}
+	?>
 </div>
 
 
