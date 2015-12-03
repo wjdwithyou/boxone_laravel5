a56594722463c8f1e2ceef3255144ecf7b518441
박용호 <dydzlwwkd@nate.com>
2015-12-03 14:20:53 +0900

    test

--- app/Http/Controllers/DeliverController.php
+++ app/Http/Controllers/DeliverController.php
@@ -157,27 +157,19 @@ class DeliverController extends Controller {
 		}
 		else if ($company == "대한통운")
 		{
+			$postdata = 'fsp_action=PARC_ACT_002&fsp_cmd=retrieveInvNoACT&invc_no=689354045702&nextpage=parcel%2Fpa_004_r.jsp';
 			
-			$postdata = http_build_query(
-				array(
-					'fsp_action' => 'PARC_ACT_002',
-					'fsp_cmd' => 'retrieveInvNoACT',
-					'invc_no' => $num,
-					'nextpage' => 'parcel/pa_004_r.jsp'
-				)
-			);
-			
-			$ch = curl_init();
+			/*$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, "https://www.doortodoor.co.kr/main/doortodoor.do");
-			//curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 			curl_setopt($ch, CURLOPT_SSLVERSION, 3);
 			curl_setopt($ch, CURLOPT_HEADER, false);
 			curl_setopt($ch, CURLOPT_POST, true);
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 20);
-			//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			//curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+			curl_setopt($ch, CURLOPT_REFERER, "https://www.doortodoor.co.kr/main/doortodoor.do");
 			curl_setopt($ch, CURLOPT_CAINFO, dirname(__FILE__)."/cacert.pem");
 			ob_start();
 			$res = curl_exec($ch);
@@ -192,40 +184,207 @@ class DeliverController extends Controller {
 				$returnVal = $buffer;
 			} 
 			curl_close($ch); 
-			echo $returnVal;
+			echo $returnVal;*/
 			
-			/*
-			$opts = array('http' =>
-				array(
+			
+			$opts = array(
+				'http' => array(
 					'method' => 'POST',
-					'header' => 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36; Content-type: application/x-www-form-urlencoded;',
-					'content' => $postdata
+					'header' => 'Content-type: application/x-www-form-urlencoded',
+					'content' => $postdata,
+					'timeout' => 10
 				), 
-				'ssl' =>
-				array(
+				'ssl' => array(
 					'verify_peer' => true,
-					'cafile' => 'http://localhost:8000/img/ca.pem',
-					'CN_match' => 'www.doortodoor.co.kr',
-					'ciphers' => 'HIGH:!SSLv2:!SSLv3' 
+					'cafile' => "http://localhost:8000/img/cacert.pem",
+					'CN_match' => 'https://www.doortodoor.co.kr/main/doortodoor.do',
+					'ciphers' => 'HIGH:!SSLv2:!SSLv3'
 				)
 			);
 			
 			$context = stream_context_create($opts);
-			$result = file_get_contents('https://www.doortodoor.co.kr/main/doortodoor.do', false, $context);*/
+			$result = file_get_contents('https://www.doortodoor.co.kr/main/doortodoor.do', false, $context);
 			
-			
+			echo $result;
 			
 		}
 		else if ($company == "한진택배")
 		{
-			var_dump(openssl_get_cert_locations());
+			
+			$opts = array(
+					'http' => array(
+							'method' => 'GET',
+							'header' => 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36; Content-type: application/x-www-form-urlencoded; charset=euc-kr',
+							'timeout' => 10
+					)
+			);
+			
+			$context = stream_context_create($opts);
+			$html = file_get_contents($url, false, $context);
+			$html = mb_convert_encoding($html, 'UTF-8', 'EUC-KR');
+			
+			$html = substr($html, strpos($html, "<tbody>"));
+			$html = substr($html, strpos($html, "bb") + 4);
+			
+			// 상품명
+			$html = substr($html, strpos($html, "bb") + 4);
+			$result['prdt'] = substr($html, 0, strpos($html, "<"));
+			
+			// 보내는 분
+			$html = substr($html, strpos($html, "bb") + 4);
+			$html = substr($html, strpos($html, "bb") + 4);
+			$html = substr($html, strpos($html, "bb") + 4);
+			$result['sender'] = trim(substr($html, 0, strpos($html, "<")));
+			
+			// 받는 분
+			$html = substr($html, strpos($html, "bb") + 4);
+			$result['receiver'] = trim(substr($html, 0, strpos($html, "<")));
+			
+			// 배송 상황 (날짜, 시간, 현재위치, 처리현황)
+			$html = substr($html, strpos($html, "<tbody>") + 4);
+			$info = array();
+			while (strpos($html, "<tr>"))
+			{
+				$temp = array();
+			
+				$html = substr($html, strpos($html, "<tr>"));
+			
+				// 날짜
+				$html = substr($html, strpos($html, "<td"));
+				$html = substr($html, strpos($html, ">") + 1);
+				$temp['date'] = substr($html, 0, strpos($html, "</td"));
+			
+				// 시간
+				$html = substr($html, strpos($html, "<td"));
+				$html = substr($html, strpos($html, ">") + 1);
+				$temp['time'] = substr($html, 0, strpos($html, "</td"));
+			
+				// 현재위치
+				$html = substr($html, strpos($html, "<td>") + 4);
+				$temp['location'] = substr($html, 0, strpos($html, "<"));
+			
+				// state
+				$html = substr($html, strpos($html, "left") + 6);
+				$temp['state'] = trim(substr($html, 0, strpos($html, "</div")), " &nbsp; ( ) <strong> </strong>");
+				
+				array_push($info, $temp);
+				
+				if (strpos($temp['state'], "배송완료") !== false)
+					break;
+			}
+				
+			array_push($result, $info);
+			
 		}
 		else if ($company == "로젠택배")
 		{
+			$html = file_get_contents($url);
+			$html = mb_convert_encoding($html, 'UTF-8', 'EUC-KR');
 				
+			// 보낸날짜
+			$html = substr($html, strpos($html, "tbTakeDt"));
+			$html = substr($html, strpos($html, "value") + 7);
+			$result['sendDate'] = substr($html, 0, strpos($html, "\""));
+			
+			// 상품명
+			$html = substr($html, strpos($html, "tbGoodsNm"));
+			$html = substr($html, strpos($html, "value") + 7);
+			$result['prdt'] = substr($html, 0, strpos($html, "\""));
+			
+			// 보내는 분
+			$html = substr($html, strpos($html, "tbSndCustNm"));
+			$html = substr($html, strpos($html, "value") + 7);
+			$result['sender'] = substr($html, 0, strpos($html, "\""));
+			
+			// 받는 분
+			$html = substr($html, strpos($html, "tbRcvCustNm"));
+			$html = substr($html, strpos($html, "value") + 7);
+			$result['sender'] = substr($html, 0, strpos($html, "\""));
+			
+			// 배송 상황 (현재위치, 처리현황)
+			$html = substr($html, strpos($html, "gridStat"));
+			$info = array();
+			while (strpos($html, "<tr"))
+			{
+				$temp = array();
+					
+				$html = substr($html, strpos($html, "<tr"));
+					
+				// 현재위치
+				$html = substr($html, strpos($html, "<td"));
+				$html = substr($html, strpos($html, ">"));
+				$temp['location'] = substr($html, 0, strpos($html, "<"));
+					
+				// state
+				$html = substr($html, strpos($html, "<td"));
+				$html = substr($html, strpos($html, ">"));
+				$temp['state'] = substr($html, 0, strpos($html, "<"));
+				
+				if (strpos($temp['state'], "&nbsp;") !== false)
+					break;
+			
+				array_push($info, $temp);
+			}
+			array_push($result, $info);
+			
+			// 받는 시간
+			$html = substr($html, strpos($html, "tbScanDt"));
+			$html = substr($html, strpos($html, "value") + 7);
+			$result['sendDate'] = substr($html, 0, strpos($html, "\""));
 		}
 		else if ($company == "현대택배")
 		{
+			/*
+			$postdata = http_build_query(
+			    array(
+			        'action' => 'processSubmit',
+			    	'invno' => 304361081183
+			    )
+			);
+			
+			$header =
+					'Content-Type: application/x-www-form-urlencoded\r\n'.
+					'Host: www.hlc.co.kr\r\n'.
+					'Origin: http://www.hlc.co.kr\r\n'.
+					'Referer: http://www.hlc.co.kr/open/tracking?invno=304361081183\r\n'.
+					'Upgrade-Insecure-Requests: 1\r\n'.
+					'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36';
+			
+			$opts = array(
+					'http' => array(
+							'method' => 'POST',
+							'header' => $header,
+							'content' => $postdata,
+							'timeout' => 10
+					)
+			);
+				
+			$context = stream_context_create($opts);
+			$result = file_get_contents('http://www.hlc.co.kr/open/tracking', false, $context);*/
+			
+			define("COOKIE_FILE", "cookie.txt");
+			
+			// Login the user
+			$ch = curl_init('http://www.hlc.co.kr/open/tracking?invno='.$num);
+			curl_setopt ($ch, CURLOPT_COOKIEJAR, COOKIE_FILE);
+			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HEADER, true);
+			echo curl_exec ($ch);
+			
+			// Read the session saved in the cookie file
+			echo "<br/><br/>";
+			$file = fopen("cookie.txt", 'r');
+			echo fread($file, 100000000);
+			echo "<br/><br/>";
+			
+			// Get the users details
+			$ch = curl_init('http://www.hlc.co.kr/open/tracking');
+			curl_setopt ($ch, CURLOPT_COOKIEJAR, COOKIE_FILE);
+			curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_HEADER, true);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, "action=processSubmit");
+			echo curl_exec ($ch);
 				
 		}
 		else if ($company == "KG옐로우캡택배")
