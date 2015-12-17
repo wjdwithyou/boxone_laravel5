c3ac689f9fa6756a78c26248f81a590d9910ee49
박용호 <dydzlwwkd@nate.com>
2015-12-17 18:04:31 +0900

    community

--- app/Http/Controllers/CommunityController.php
+++ app/Http/Controllers/CommunityController.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
+use App\Http\models\CommunityModel;
+use Request;
 
 class CommunityController extends Controller {
 
@@ -19,8 +21,56 @@ class CommunityController extends Controller {
 	
 	public function index()
 	{
+		$cmModel = new CommunityModel();
+		
+		if (Request::has('cate'))
+			$cate = Request::input('cate');
+		else
+			$cate = "전체";
+		
+		$cateL = $cmModel->getLargeCategory();
+		
+		if ($cate == "전체")
+			$cateS = $cmModel->getLargeCategory();
+		else
+			$cateS = $cmModel->getSmallCategory($cate);		
+		
 		$page = 'community';
-		return view($page, array('page' => $page));
+		return view($page, array('page' => $page, 'cate' => $cate, 'cateL' => $cateL['data'], 'cateS' => $cateS['data']));
+	}
+	
+	public function getInfo()
+	{
+		$cmModel = new CommunityModel();
+		
+		$cate = json_decode(Request::input('cate'));
+		$adr_img = Request::input('adr_img');
+		$page_type = Request::input('page_type');
+		$paging = Request::input('paging');
+		
+		if (!is_numeric($cate[0]))
+		{
+			$temp = array();
+			
+			if ($cate[0] != "전체")
+			{				
+				foreach($cate as $list)
+				{
+					$data = $cmModel->getSmallCategory($list);
+					foreach($data['data'] as $idx)
+						array_push($temp, $idx->idx);
+				}
+			}
+			$cate = $temp;
+		}
+
+		$result = $cmModel->getInfoList($cate, $paging);
+		
+		
+		//print_r ($result);
+		
+		$page = 'communityInfo';
+		return view($page, array('page' => $page, 'result' => $result['data'], 'adr_img' => $adr_img, 'page_type' => $page_type, 'paging' => $result['paging']));
 	}
 	
 	public function write()
