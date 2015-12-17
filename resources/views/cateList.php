676aeb97ed22cc770362d06ee3bfbce457161f41
박용호 <dydzlwwkd@nate.com>
2015-12-18 08:59:29 +0900

    cateList

--- resources/views/cateList.php
+++ resources/views/cateList.php
@@ -11,7 +11,7 @@
 <?php foreach($result1 as $list1):?>
 <Category>
 	<CustID>boxone</CustID>
-	<Cinc>l<?= $list1->idx?>m0s0</Cinc>
+	<Cinc><?= $list1->idx?>00000</Cinc>
 	<Ccode1><?= $list1->idx?></Ccode1>
 	<Ccode2>0</Ccode2>
 	<Ccode3>0</Ccode3>
@@ -23,7 +23,7 @@
 <?php foreach($result2 as $list2):?>
 <Category>
 	<CustID>boxone</CustID>
-	<Cinc>l<?= $list2->large_idx?>m<?= $list2->idx?>s0</Cinc>
+	<Cinc><?= $list2->large_idx?><?php echo sprintf("%02d",$list2->idx);?>000</Cinc>
 	<Ccode1><?= $list2->large_idx?></Ccode1>
 	<Ccode2><?= $list2->idx?></Ccode2>
 	<Ccode3>0</Ccode3>
@@ -35,7 +35,7 @@
 <?php foreach($result3 as $list3):?>
 <Category>
 	<CustID>boxone</CustID>
-	<Cinc>l<?= $list3->lidx?>m<?= $list3->midx?>s<?= $list3->sidx?></Cinc>
+	<Cinc><?= $list3->lidx?><?php echo sprintf("%02d",$list3->midx);?><?php echo sprintf("%03d",$list3->sidx);?></Cinc>
 	<Ccode1><?= $list3->lidx?></Ccode1>
 	<Ccode2><?= $list3->midx?></Ccode2>
 	<Ccode3><?= $list3->sidx?></Ccode3>
