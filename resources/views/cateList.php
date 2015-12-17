453dd2ba9a4a85450d6a388104c1f727a0289b27
박용호 <dydzlwwkd@nate.com>
2015-12-17 13:22:23 +0900

    cateList

--- resources/views/cateList.php
+++ resources/views/cateList.php
@@ -10,7 +10,7 @@
 
 <?php foreach($result1 as $list1):?>
 <Category>
-	<Cust ID>boxone</Cust ID>
+	<CustID>boxone</CustID>
 	<Cinc>l<?= $list1->idx?>m0s0</Cinc>
 	<Ccode1><?= $list1->idx?></Ccode1>
 	<Ccode2>0</Ccode2>
@@ -22,7 +22,7 @@
 
 <?php foreach($result2 as $list2):?>
 <Category>
-	<Cust ID>boxone</Cust ID>
+	<CustID>boxone</CustID>
 	<Cinc>l<?= $list2->large_idx?>m<?= $list2->idx?>s0</Cinc>
 	<Ccode1><?= $list2->large_idx?></Ccode1>
 	<Ccode2><?= $list2->idx?></Ccode2>
@@ -34,7 +34,7 @@
 	
 <?php foreach($result3 as $list3):?>
 <Category>
-	<Cust ID>boxone</Cust ID>
+	<CustID>boxone</CustID>
 	<Cinc>l<?= $list3->lidx?>m<?= $list3->midx?>s<?= $list3->sidx?></Cinc>
 	<Ccode1><?= $list3->lidx?></Ccode1>
 	<Ccode2><?= $list3->midx?></Ccode2>
