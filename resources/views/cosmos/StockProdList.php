0036b96dfef0d0c54dd4fc6370e82e798b38aa55
박용호 <dydzlwwkd@nate.com>
2016-01-21 18:06:33 +0900

    test

---
+++ resources/views/cosmos/StockProdList.php
@@ -0,0 +1,28 @@
+<?php
+	header("Content-Type: text/xml; charset=utf-8");
+	$mallList = DB::connection('sqlsrv')->select("SELECT MallKind, MallID FROM cgMallMain");
+	$data = array();
+	foreach($mallList as $list)
+		array_push($data, DB::connection('sqlsrv')->select("SELECT MallKind, MallID, Pcode, ProdInc, Pname, Lprice, Sprice, Stock, Purl, Pimg, Ccode1, Ccode2, Ccode3, Ccode4 FROM cgProdMain_$list"));
+?>
+
+<table border=1>
+	<?php foreach ($data as $list) :?>
+	<tr>
+		<td class=MallKind><?=$list->MallKind?></td>
+		<td class=MallID><?=$list->MallID?></td>
+		<td class=Pcode>0</td>
+		<td class=ProdInc><?=$list->ProdInc?></td>
+		<td class=Pname><?=$list->PnameP?></td>
+		<td class=Lprice><?=$list->Lprice?></td>
+		<td class=Sprice><?=$list->Sprice?></td>
+		<td class=Stock><?=$list->Stock?></td>
+		<td class=Purl><?=$list->PurlP?></td>
+		<td class=Pimg><?=$list->PimgP?></td>
+		<td class=Ccode1><?=$list->Ccode1?></td>
+		<td class=Ccode2><?=$list->Ccode2?></td>
+		<td class=Ccode3><?=$list->Ccode3?></td>
+		<td class=Ccode4><?=$list->Ccode4?></td>
+	</tr>
+	<?php endforeach;?>
+</table>
